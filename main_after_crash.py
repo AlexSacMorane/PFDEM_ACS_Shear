7e8cee186c2f7a25ff4d408aa42e5083074af7b5
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-03-20 12:57:47 +0100

    Create main_after_crash.py

---
+++ main_after_crash.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Alexandre Sac--Morane
+alexandre.sac-morane@uclouvain.be
+
+This is the file to restart a simulation after a crash.
+"""
+
+#-------------------------------------------------------------------------------
+#Librairy
+#-------------------------------------------------------------------------------
+
+import os
+import pickle
+import shutil
+from datetime import datetime
+from pathlib import Path
+
+#Own function and class
+import main
+import Create_IC
+import Create_IC_Polygonal
+import Contact_gg
+import Contact_gimage
+import Grain
+import Shear_Polygonal
+import Report
+import User
+import Owntools
+import Owntools.Plot
+import Owntools.Save
+
+#-------------------------------------------------------------------------------
+#User
+#-------------------------------------------------------------------------------
+
+name_to_load = 'Run_1_ic_save_dicts'
+
+#-------------------------------------------------------------------------------
+#load data
+#-------------------------------------------------------------------------------
+
+toload = open(name_to_load,'rb')
+dict_save = pickle.load(toload)
+toload.close()
+dict_algorithm = dict_save['algorithm']
+dict_geometry = dict_save['geometry']
+dict_material = dict_save['material']
+dict_sample = dict_save['sample']
+dict_sollicitations = dict_save['sollicitations']
+simulation_report = dict_save['report']
+
+#-------------------------------------------------------------------------------
+#plan simulation
+#-------------------------------------------------------------------------------
+
+simulation_report.write('\nA crash occurs...\n\n')
+
+#-------------------------------------------------------------------------------
+#main
+#-------------------------------------------------------------------------------
+
+if name_to_load[-14:] =='_ic_save_dicts':
+    main.shear_sample(dict_algorithm, dict_material, dict_sample, dict_sollicitations, simulation_report)
+
+#-------------------------------------------------------------------------------
+#close simulation
+#-------------------------------------------------------------------------------
