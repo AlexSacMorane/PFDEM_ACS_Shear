f22e202e21b5ad4183949d43dd781301e56db353
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-03-20 12:59:07 +0100

    Update Save.py

--- Owntools/Save.py
+++ Owntools/Save.py
@@ -11,14 +11,43 @@ This file contains the different functions to save data.
 #-------------------------------------------------------------------------------
 
 import pickle
+
 #Own
-import Contact
-import Contact_gw
+import Contact_gg
+import Contact_gimage
 import Grain
 import Report
 
 #-------------------------------------------------------------------------------
 #functions
+#-------------------------------------------------------------------------------
+
+def save_dicts_ic(dict_algorithm, dict_geometry, dict_material, dict_sample, dict_sollicitations, simulation_report):
+    """
+    Save dictionnaries after the initial configuration.
+
+        Input :
+            an algorithm dictionnary (a dict)
+            a geometry dictionnary (a dict)
+            a material dictionnary (a dict)
+            a sample dictionnary (a dict)
+            a sollicitations dictionnary (a dict)
+            a simulation report (a report)
+        Output :
+            Nothing, but a save file is generated (a file)
+    """
+    outfile = open(dict_algorithm['name_folder']+'_ic_save_dicts','wb')
+    dict_save = {}
+    dict_save['algorithm'] = dict_algorithm
+    dict_save['geometry'] = dict_geometry
+    dict_save['material'] = dict_material
+    dict_save['sample'] = dict_sample
+    dict_save['sollicitations'] = dict_sollicitations
+    dict_save['report'] = simulation_report
+    pickle.dump(dict_save,outfile)
+    outfile.close()
+
+
 #-------------------------------------------------------------------------------
 
 def save_dicts(dict_algorithm, dict_geometry, dict_material, dict_sample, dict_sollicitations, dict_tracker, simulation_report):
@@ -72,52 +101,5 @@ def save_dicts_before_pf(dict_algorithm, dict_geometry, dict_material, dict_samp
     dict_save['sollicitations'] = dict_sollicitations
     dict_save['tracker'] = dict_tracker
     dict_save['report'] = simulation_report
-    pickle.dump(dict_save,outfile)
-    outfile.close()
-
-#-------------------------------------------------------------------------------
-
-def save_tempo(dict_algorithm,dict_tracker):
-    """
-    Save trackers and configuration during  PFDEM iteration.
-
-        Input :
-            an algorithm dictionnary (a dict)
-            a tracker dictionnary (a dict)
-        Output :
-            Nothing, but a save file is generated (a file)
-    """
-    outfile = open('../'+dict_algorithm['main_folder_name']+'/'+dict_algorithm['name_folder']+'_save_tempo','wb')
-    dict_save = {}
-    dict_save['k0_xmin_L'] = dict_tracker['k0_xmin_L']
-    dict_save['k0_xmax_L'] = dict_tracker['k0_xmax_L']
-    dict_save['S_dissolved_L'] = dict_tracker['S_dissolved_L']
-    dict_save['S_grains_dissolvable_L'] = dict_tracker['S_grains_dissolvable_L']
-    dict_save['S_dissolved_perc_L'] = dict_tracker['S_dissolved_perc_L']
-    dict_save['S_dissolved_perc_dissolvable_L'] = dict_tracker['S_dissolved_perc_dissolvable_L']
-    dict_save['S_grains_L'] = dict_tracker['S_grains_L']
-    pickle.dump(dict_save,outfile)
-    outfile.close()
-
-#-------------------------------------------------------------------------------
-
-def save_final(dict_algorithm,dict_tracker):
-    """
-    Save trackers and configuration at the end of simulation.
-
-        Input :
-            an algorithm dictionnary (a dict)
-            a tracker dictionnary (a dict)
-        Output :
-            Nothing, but a save file is generated (a file)
-    """
-    os.remove('../'+dict_algorithm['main_folder_name']+'/'+dict_algorithm['name_folder']+'_save_tempo')
-    outfile = open('../'+dict_algorithm['main_folder_name']+'/'+dict_algorithm['name_folder']+'_save','wb')
-    dict_save = {}
-    dict_save['k0_xmin_L'] = dict_tracker['k0_xmin_L']
-    dict_save['k0_xmax_L'] = dict_tracker['k0_xmax_L']
-    dict_save['S_dissolved_L'] = dict_tracker['S_dissolved_L']
-    dict_save['S_dissolved_perc_L'] = dict_tracker['S_dissolved_perc_L']
-    dict_save['S_grains_L'] = dict_tracker['S_grains_L']
     pickle.dump(dict_save,outfile)
     outfile.close()
