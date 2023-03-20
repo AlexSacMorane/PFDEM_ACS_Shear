d6d05077e36ee122dd065293a4f5b208b17e0656
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-03-20 12:58:41 +0100

    glow up

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import os
 import shutil
 from datetime import datetime
 from pathlib import Path
-import pickle
 
 #Own function and class
 import Create_IC
@@ -27,115 +26,192 @@ import Report
 import User
 import Owntools
 import Owntools.Plot
+import Owntools.Save
 
 #-------------------------------------------------------------------------------
-#Plan simulation
+#Functions
 #-------------------------------------------------------------------------------
 
-#get data
-dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations = User.All_parameters()
+def plan_simulation():
+    """
+    Plan the simulation :
+        - Get data
+        - Create folders and files
+        - Find a name for the simulation
 
-#debug folder
-if Path('Debug').exists():
-    shutil.rmtree('Debug')
-os.mkdir('Debug')
-if dict_algorithm['Debug'] or dict_algorithm['Debug_DEM'] or dict_ic['Debug_DEM_IC'] :
-    if dict_algorithm['Debug'] :
-        os.mkdir('Debug/Configuration')
-    if dict_algorithm['Debug_DEM'] :
-        os.mkdir('Debug/Shear')
-    if dict_ic['Debug_DEM_IC'] :
-        os.mkdir('Debug/Init_disks')
-        os.mkdir('Debug/Init_polygons')
+        Input :
+            Nothing
+        Output :
+            an algorithm dictionnary (a dict)
+            a geometry dictionnary (a dict)
+            an initial condition dictionnary (a dict)
+            a material dictionnary (a dict)
+            a sample dictionnary (a dict)
+            a sollicitations dictionnary (a dict)
+            a simulation report (a report)
+    """
+    #get data
+    dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations = User.All_parameters()
 
-#report
-simulation_report = Report.Report('Debug/Report.txt',datetime.now())
-simulation_report.tic_tempo(datetime.now())
+    #debug folder
+    if Path('Debug').exists():
+        shutil.rmtree('Debug')
+    os.mkdir('Debug')
+    if dict_algorithm['Debug'] or dict_algorithm['Debug_DEM'] or dict_ic['Debug_DEM_IC'] :
+        if dict_algorithm['Debug'] :
+            os.mkdir('Debug/Configuration')
+        if dict_algorithm['Debug_DEM'] :
+            os.mkdir('Debug/Shear')
+        if dict_ic['Debug_DEM'] :
+            os.mkdir('Debug/Init_disks')
+            os.mkdir('Debug/Init_polygons')
 
-#find name
-if dict_algorithm['SaveData'] :
-    #check if save folder exists
-    if not Path('../'+dict_algorithm['main_folder_name']).exists():
-        shutil.mkdir('../'+dict_algorithm['main_folder_name'])
+    #report
+    simulation_report = Report.Report('Debug/Report.txt',datetime.now())
 
-    i_run = 1
-    folderpath = Path('../'+dict_algorithm['main_folder_name']+'/'+dict_algorithm['template_simulation_name']+str(i_run))
-    while folderpath.exists():
-        i_run = i_run + 1
+    #find name
+    if dict_algorithm['SaveData'] :
+        #check if save folder exists
+        if not Path('../'+dict_algorithm['main_folder_name']).exists():
+            shutil.mkdir('../'+dict_algorithm['main_folder_name'])
+        i_run = 1
         folderpath = Path('../'+dict_algorithm['main_folder_name']+'/'+dict_algorithm['template_simulation_name']+str(i_run))
-    dict_algorithm['name_folder'] = dict_algorithm['template_simulation_name']+str(i_run)
-else :
-    dict_algorithm['name_folder'] = 'One_Run'
+        while folderpath.exists():
+            i_run = i_run + 1
+            folderpath = Path('../'+dict_algorithm['main_folder_name']+'/'+dict_algorithm['template_simulation_name']+str(i_run))
+        dict_algorithm['name_folder'] = dict_algorithm['template_simulation_name']+str(i_run)
+    else :
+        dict_algorithm['name_folder'] = 'One_Run'
+
+    return dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report
 
 #-------------------------------------------------------------------------------
-#IC simulation
-#-------------------------------------------------------------------------------
 
-Create_IC.LG_tempo(dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report)
+def generate_ic(dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report) :
+    """
+    Generate an initial condition.
 
-simulation_report.tac_tempo(datetime.now(), 'Loading with disks')
+    First, disks are generated and loaded. Once a steady-state is detected, grains are discretized.
+    Then, polygons are loaded until a steady-state is detected.
 
-#-------------------------------------------------------------------------------
-#Discretize grains
-#-------------------------------------------------------------------------------
+        Input :
+            an algorithm dictionnary (a dict)
+            a geometry dictionnary (a dict)
+            an initial condition dictionnary (a dict)
+            a material dictionnary (a dict)
+            a sample dictionnary (a dict)
+            a sollicitations dictionnary (a dict)
+            a simulation report (a report)
+        Output :
+            Nothing, but dictionnaries are updated
+    """
+    #generate and load disks
+    simulation_report.tic_tempo(datetime.now())
+    Create_IC.LG_tempo(dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report)
+    simulation_report.tac_tempo(datetime.now(), 'Loading with disks')
 
-#change Parameters
-simulation_report.tic_tempo(datetime.now())
-dict_ic['i_DEM_stop_IC'] = 6000
-dict_ic['i_print_plot_IC'] = 100
+    simulation_report.write_and_print('Discretize the sample\n', 'Discretize the sample')
+    #change Parameters
+    dict_ic['i_DEM_stop_IC'] = 6000
+    dict_ic['i_print_plot_IC'] = 100
 
-simulation_report.write_and_print('Discretize the sample\n', 'Discretize the sample')
+    #discretization
+    simulation_report.tic_tempo(datetime.now())
+    dict_ic = Create_IC_Polygonal.Discretize_Grains(dict_ic, dict_geometry['discretization']) #overwrite sphere dict_ic
+    simulation_report.tac_tempo(datetime.now(), 'From disks to polygons')
 
-dict_ic = Create_IC_Polygonal.Discretize_Grains(dict_ic, 80) #overwrite sphere dict_ic
-
-simulation_report.tac_tempo(datetime.now(), 'From disks to polygons')
-simulation_report.tic_tempo(datetime.now())
-
-#load discrete grains
-Create_IC_Polygonal.DEM_loading(dict_algorithm, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report)
-simulation_report.tac_tempo(datetime.now(), 'Loading with polygons')
-
-#-------------------------------------------------------------------------------
-#Define group
-#-------------------------------------------------------------------------------
-
-simulation_report.tic_tempo(datetime.now())
-
-#define packs
-i_bottom = 0
-i_top = 0
-for grain in dict_ic['L_g_tempo'] :
-    if grain.is_group(0, 2*dict_geometry['R_mean'], 'Bottom') :
-        i_bottom = i_bottom + 1
-    elif grain.is_group(dict_sample['y_box_max']-2*dict_geometry['R_mean'], dict_sample['y_box_max'], 'Top') :
-        i_top = i_top + 1
-simulation_report.write_and_print(str(i_bottom)+' grains in Bottom group\n'+str(i_top)+' grains in Top group\n\n', str(i_bottom)+' grains in Bottom group\n'+str(i_top)+' grains in Top group\n')
-
-#plot group distribution
-Owntools.Plot.Plot_group_distribution(dict_ic)
-
-#delete contact gw
-dict_ic['L_contact_gw'] = []
-dict_ic['L_contact_gw_ij'] = []
-
-simulation_report.tac_tempo(datetime.now(), 'Define groups')
+    #load discrete grains
+    simulation_report.tic_tempo(datetime.now())
+    Create_IC_Polygonal.DEM_loading(dict_algorithm, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report)
+    simulation_report.tac_tempo(datetime.now(), 'Loading with polygons')
 
 #-------------------------------------------------------------------------------
-#From ic to current
+
+def define_group(dict_geometry, dict_ic, dict_sample, simulation_report):
+    """
+    Define top and bottom groups and walls are deleted.
+
+        Input :
+             a geometry dictionnary (a dict)
+             an initial dictionnary (a dict)
+             a sample dictionnary (a dict)
+             a simulation report (a report)
+         Output :
+            Nothing, but dictionnaries are updated
+    """
+    #define packs
+    simulation_report.tic_tempo(datetime.now())
+    i_bottom = 0
+    i_top = 0
+    for grain in dict_ic['L_g_tempo'] :
+        if grain.is_group(0, 2*dict_geometry['R_mean'], 'Bottom') :
+            i_bottom = i_bottom + 1
+        elif grain.is_group(dict_sample['y_box_max']-2*dict_geometry['R_mean'], dict_sample['y_box_max'], 'Top') :
+            i_top = i_top + 1
+    simulation_report.write_and_print(str(i_bottom)+' grains in Bottom group\n'+str(i_top)+' grains in Top group\n\n', str(i_bottom)+' grains in Bottom group\n'+str(i_top)+' grains in Top group\n')
+
+    #delete contact gw
+    dict_ic['L_contact_gw'] = []
+    dict_ic['L_contact_gw_ij'] = []
+
+    #plot group distribution
+    Owntools.Plot.Plot_group_distribution(dict_ic)
+    simulation_report.tac_tempo(datetime.now(), 'Define groups')
+
 #-------------------------------------------------------------------------------
 
-#make conversion
+def from_ic_to_real(dict_algorithm, dict_geometry, dict_material, dict_sample, dict_sollicitations, simulation_report):
+    """
+    Convert initial configuration to a current one.
+
+        Input :
+            an algorithm dictionnary (a dict)
+            a geometry dictionnary (a dict)
+            an initial condition dictionnary (a dict)
+            a material dictionnary (a dict)
+            a sample dictionnary (a dict)
+            a sollicitations dictionnary (a dict)
+            a simulation report (a report)
+        Output :
+            Nothing, but dictionnaries are updated
+    """
+    Owntools.convert_ic_to_real(dict_ic, dict_sample)
+    #save
+    Owntools.Save.save_dicts_ic(dict_algorithm, dict_geometry, dict_material, dict_sample, dict_sollicitations, simulation_report)
 
 #-------------------------------------------------------------------------------
-#Shear simulation
+
+def shear_sample(dict_algorithm, dict_material, dict_sample, dict_sollicitations, simulation_report):
+    '''
+    Shear the sample.
+
+        Input :
+            an algorithm dictionnary (a dict)
+            a geometry dictionnary (a dict)
+            an initial condition dictionnary (a dict)
+            a material dictionnary (a dict)
+            a sample dictionnary (a dict)
+            a sollicitations dictionnary (a dict)
+            a simulation report (a report)
+        Output :
+            Nothing, but dictionnaries are updated
+    '''
+    simulation_report.write_and_print('Shearing the sample\n', 'Shearing the sample')
+    #change Parameters
+    dict_algorithm['i_DEM'] = 0
+
+    #shear sample
+    simulation_report.tic_tempo(datetime.now())
+    Shear_Polygonal.DEM_shear_load(dict_algorithm, dict_material, dict_sample, dict_sollicitations, simulation_report)
+    simulation_report.tac_tempo(datetime.now(), 'Shearing')
+
+#-------------------------------------------------------------------------------
+#main
 #-------------------------------------------------------------------------------
 
-#change Parameters
-simulation_report.tic_tempo(datetime.now())
-simulation_report.write_and_print('Shearing the sample\n', 'Shearing the sample')
-dict_sample['i_DEM'] = 0
-
-#shear sample
-Shear_Polygonal.DEM_shear_load(dict_algorithm, dict_material, dict_sample, dict_sollicitations, simulation_report)
-
-simulation_report.tac_tempo(datetime.now(), 'Shearing')
+if '__main__' == __name__:
+    dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report = plan_simulation()
+    generate_ic(dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report)
+    define_group(dict_geometry, dict_ic, dict_sample, simulation_report)
+    from_ic_to_real(dict_algorithm, dict_geometry, dict_material, dict_sample, dict_sollicitations, simulation_report)
+    shear_sample(dict_algorithm, dict_material, dict_sample, dict_sollicitations, simulation_report)
