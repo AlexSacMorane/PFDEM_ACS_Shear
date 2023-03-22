ed2d1e35c935f26d71389681e68fc04afe6cfb21
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-03-22 11:33:36 +0100

    add save debug

--- Owntools/Save.py
+++ Owntools/Save.py
@@ -22,6 +22,37 @@ import Report
 #functions
 #-------------------------------------------------------------------------------
 
+def save_dicts_debug(dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report):
+    """
+    Save dictionnaries after the initial configuration.
+
+    To be deleted. Only for debug
+
+        Input :
+            an algorithm dictionnary (a dict)
+            a geometry dictionnary (a dict)
+            an initial condition dictionnary (a dict)
+            a material dictionnary (a dict)
+            a sample dictionnary (a dict)
+            a sollicitations dictionnary (a dict)
+            a simulation report (a report)
+        Output :
+            Nothing, but a save file is generated (a file)
+    """
+    outfile = open(dict_algorithm['name_folder']+'_db_save_dicts','wb')
+    dict_save = {}
+    dict_save['algorithm'] = dict_algorithm
+    dict_save['geometry'] = dict_geometry
+    dict_save['ic'] = dict_ic
+    dict_save['material'] = dict_material
+    dict_save['sample'] = dict_sample
+    dict_save['sollicitations'] = dict_sollicitations
+    dict_save['report'] = simulation_report
+    pickle.dump(dict_save,outfile)
+    outfile.close()
+
+#-------------------------------------------------------------------------------
+
 def save_dicts_ic(dict_algorithm, dict_geometry, dict_ic, dict_material, dict_sample, dict_sollicitations, simulation_report):
     """
     Save dictionnaries after the initial configuration.
