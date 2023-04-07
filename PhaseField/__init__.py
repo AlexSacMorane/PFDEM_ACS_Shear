80c463fa19b7a5f8cccef55aa329bd78fd52b041
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-04-07 11:01:45 +0200

    create PhaseField module

--- PhaseField/__init__.py
+++ PhaseField/__init__.py
@@ -14,3 +14,32 @@ This file contains functions used in the simulation to create and run phase fiel
 #-------------------------------------------------------------------------------
 #Function
 #-------------------------------------------------------------------------------
+
+def pf_simulation():
+    """
+    Convert from DEM, prepare simulation, run simulation and convert to DEM.
+
+        Input :
+
+        Output :
+
+    """
+    #compute E_mec
+
+    #compute kc
+
+    #write etas
+
+    #write solute
+
+    #write kc
+
+    #write emec
+
+    #write .i
+
+    #run simulation
+
+    #sort files
+
+    #pf to dem
